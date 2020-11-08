c1a92bc09b6c7114ad40b2b9072b11356fa26a2d
DESKTOP-LMSNJM8\w10 <yuliyakuxenok@gmail.com>
2020-11-08 18:24:25 +0700

    Добавьте файлы проекта.

---
+++ WebRequest.Specs/Features/GettingAPI.feature
@@ -0,0 +1,7 @@
+﻿Feature: Getting web API
+
+@mytag
+Scenario: Sending request for getting API
+	Given I have an API web page
+	When I send request to get API web 'https://boyns97jh5.execute-api.eu-west-1.amazonaws.com/Prod/swagger/v1/swagger.json'
+	Then the result should have status code '200'
---
+++ WebRequest.Specs/StepDefinitions/GettingWebAPISteps.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+using Library.Specs.Drivers;
+
+namespace Library.Specs.StepDefinitions
+{
+    [Binding]
+    public class GettingWebAPISteps
+    {
+        private readonly WebDriver _webDriver;
+
+        public GettingWebAPISteps(WebDriver webDriver)
+        {
+            _webDriver = webDriver;
+        }
+
+        [Given(@"I have an API web page")]
+        public void GivenIHaveAnAPIWebPage()
+        {
+            _webDriver.InitializeHttpClient();
+        }
+        
+        [When(@"I send request to get API web '(.*)'")]
+        public async Task WhenISendRequestToGetAPIWeb(string url)
+        {
+            await _webDriver.HttpClientGet(url);
+        }
+
+        [Then(@"the result should have status code '(.*)'")]
+        public void ThenTheResultShouldHaveStatusCode(int expectedStatusCode)
+        {
+            _webDriver.CheckResponseStatusCode(expectedStatusCode);
+        }
+    }
+}
