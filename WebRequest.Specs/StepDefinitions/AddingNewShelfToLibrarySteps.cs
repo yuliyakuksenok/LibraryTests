207877ff1fabce8fdd4c2ff01f75c5316c69949d
yuliyakuksenok <74145550+yuliyakuksenok@users.noreply.github.com>
2020-11-09 16:25:48 +0700

    Add files via upload

---
+++ WebRequest.Specs/StepDefinitions/AddingNewShelfToLibrarySteps.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace Library.Specs.StepDefinitions
+{
+    [Binding]
+    public class ShelfSteps
+    {
+        [Given(@"API v(.*)")]
+        public void GivenAPIV(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"Client is authorized in librarian role")]
+        public void GivenClientIsAuthorizedInLibrarianRole()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I send the request for adding a shelf to ""(.*)""")]
+        public void WhenISendTheRequestForAddingAShelfTo(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"I get the successful response with code '(.*)'")]
+        public void ThenIGetTheSuccessfulResponseWithCode(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"new shelf was added")]
+        public void ThenNewShelfWasAdded()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
---
+++ WebRequest.Specs/StepDefinitions/ListOfShelfSteps.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace Library.Specs.StepDefinitions
+{
+    [Binding]
+    public class ShelfSteps
+    {
+        [When(@"I send request for getting list of shelves to ""(.*)""")]
+        public void WhenISendRequestForGettingListOfShelvesTo(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"list of shelves is displayed")]
+        public void ThenListOfShelvesIsDisplayed()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
